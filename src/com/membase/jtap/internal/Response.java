ee29bdffd3a3f370043408bf1139146961673bab
Mike Wiederhold <mikewied@comcast.net>
2011-01-24 19:14:49 -0800

    Added documentation for the Response class

--- src/com/membase/jtap/internal/Response.java
+++ src/com/membase/jtap/internal/Response.java
@@ -2,19 +2,43 @@ package com.membase.jtap.internal;
 
 import java.nio.ByteBuffer;
 
+/**
+ * Holds ByteBuffers that have been read from the tap stream
+ */
 public class Response {
+	
+	/**
+	 * The bytes that were read from the tap stream
+	 */
 	private ByteBuffer buffer;
+	
+	/**
+	 * The length of the buffer holding the bytes red from the tap stream
+	 */
 	private int bufferLength;
 	
+	/**
+	 * Creates an object that holds bytes read from the tap stream
+	 * @param buffer
+	 * @param bufferLength
+	 */
 	public Response(ByteBuffer buffer, int bufferLength) {
 		this.buffer = buffer;
 		this.bufferLength = bufferLength;
 	}
 	
+	/**
+	 * Gets the bytes read from the tap stream
+	 * @return The bytes read from the tap stream
+	 */
 	public ByteBuffer getBuffer() {
 		return buffer;
 	}
 	
+	/**
+	 * Gets the number of bytes read from the tap stream
+	 * @return The length of the buffer
+	 */
 	public int getBufferLength() {
 		return bufferLength;
 	}
