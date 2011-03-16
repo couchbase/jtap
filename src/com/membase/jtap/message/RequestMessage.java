01e29d645c253db31532c026baf9a16fbaef1edb
Mike Wiederhold <mikewied@comcast.net>
2011-03-16 12:01:48 -0700

    Added documentation for RequestMessage.java
    Change-Id: I40b575b3b993574fca80412dd7f6006288bd2233

--- src/com/membase/jtap/message/RequestMessage.java
+++ src/com/membase/jtap/message/RequestMessage.java
@@ -8,6 +8,10 @@ import org.slf4j.LoggerFactory;
 import com.membase.jtap.TapStreamClient;
 import com.membase.jtap.internal.Util;
 
+/**
+ * A tap request message that is used to start tap streams, perform sasl authentication, and
+ * maintain the health of tap streams.
+ */
 public class RequestMessage extends HeaderMessage{
 	private static final Logger LOG = LoggerFactory.getLogger(RequestMessage.class);
 	
@@ -21,6 +25,9 @@ public class RequestMessage extends HeaderMessage{
 	private byte[] vbucketlist;
 	private byte[] value;
 	
+	/**
+	 * Create a tap request message. These messages are used to start tap streams.
+	 */
 	public RequestMessage() {
 		flags = new byte[0];
 		name = new byte[0];
@@ -29,6 +36,11 @@ public class RequestMessage extends HeaderMessage{
 		value = new byte[0];
 	}
 	
+	/**
+	 * Sets the flags for the tap stream. These flags decide what kind of tap stream 
+	 * will be received.
+	 * @param f The flags to use for this tap stream.
+	 */
 	public void setFlags(Flag f) {
 		if (flags.length != FLAGS_FIELD_LENGTH)
 			flags = new byte[FLAGS_FIELD_LENGTH];
@@ -38,6 +50,10 @@ public class RequestMessage extends HeaderMessage{
 		}
 	}
 	
+	/**
+	 * Stream all keys inserted into the server after a given date.
+	 * @param date - The date to stream keys from. Null to stream all keys.
+	 */
 	public void setBackfill(Date date) {
 		backfilldate = new byte[BACKFILL_DATE_FIELD_LENGTH];
 		if (date == null) {
@@ -49,12 +65,20 @@ public class RequestMessage extends HeaderMessage{
 		encode();
 	}
 	
+	/**
+	 * Returns the flags for this message.
+	 * @return An int value of flags set for this tap message.
+	 */
 	public int getFlags() {
 		if (flags.length != FLAGS_FIELD_LENGTH)
 			return 0;
 		return (int) Util.fieldToValue(flags, 0, FLAGS_FIELD_LENGTH);
 	}
 	
+	/**
+	 * Sets a list of vbuckets to stream keys from.
+	 * @param vbs - A list of vbuckets.
+	 */
 	public void setVbucketlist(int[] vbs) {
 		byte[] vblist = new byte[(vbs.length + 1) * VBUCKET_LIST_FIELD_LENGTH];
 		for (int i = 0; i < vbs.length + 1; i++) {
@@ -69,6 +93,11 @@ public class RequestMessage extends HeaderMessage{
 		encode();
 	}
 	
+	/**
+	 * Sets a name for this tap stream. If the tap strem fails this name can be used to try to restart
+	 * the tap stream from where it last left off.
+	 * @param s The name for the tap stream.
+	 */
 	public void setName(String s) {
 		// TODO: This needs to throw an exception
 		long len = s.length();
@@ -79,11 +108,18 @@ public class RequestMessage extends HeaderMessage{
 		encode();
 	}
 	
+	/**
+	 * Used to set an extra value when doing sasl authentication.
+	 * @param s The value to be set.
+	 */
 	public void setValue(String s) {
 		value = s.getBytes();
 		encode();
 	}
 	
+	/**
+	 * Encodes the message into binary.
+	 */
 	private void encode() {
 		byte[] buffer = new byte[HEADER_LENGTH + name.length + flags.length + vbucketlist.length + backfilldate.length + value.length];
 		
