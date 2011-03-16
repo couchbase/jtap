61dfd804b454eccd35c9e56856d040218ed84fae
Mike Wiederhold <mikewied@comcast.net>
2011-03-16 12:11:35 -0700

    Added documentation for TextExporter.java
    Change-Id: I103f4e9634c75ef10f65176cde7ee4921425f2f0

--- src/com/membase/jtap/exporter/TextExporter.java
+++ src/com/membase/jtap/exporter/TextExporter.java
@@ -3,12 +3,19 @@ package com.membase.jtap.exporter;
 import com.membase.jtap.exception.FieldDoesNotExistException;
 import com.membase.jtap.message.ResponseMessage;
 
+/**
+ * Exports keys and their value to standard out.
+ */
 public class TextExporter implements Exporter {
 	
 	public TextExporter() {
 
 	}
 	
+	/**
+	 * Prints a key name and attempts to print its value if it exists to
+	 * standard out.
+	 */
 	@Override
 	public void write(ResponseMessage message) {
 		try {
