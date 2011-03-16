ef232b7d6018891c5701d14542062f194c87f51d
Mike Wiederhold <mikewied@comcast.net>
2011-03-16 11:02:17 -0700

    Added documentation for Exporter.java
    Change-Id: Ie5af8372b40c0af05c35dad7d9bb75f6d1108e33

--- src/com/membase/jtap/exporter/Exporter.java
+++ src/com/membase/jtap/exporter/Exporter.java
@@ -2,7 +2,18 @@ package com.membase.jtap.exporter;
 
 import com.membase.jtap.message.ResponseMessage;
 
+/**
+ * An interface for exporting data from a tap stream.
+ */
 public interface Exporter {
+	/**
+	 * Specifies writing tap message information to a given output.
+	 * @param message The tap message to write.
+	 */
 	public void write(ResponseMessage message);
+	
+	/**
+	 * Closes the file descriptor used by this exporter.
+	 */
 	public void close();
 }
